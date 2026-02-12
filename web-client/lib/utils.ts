61df4addfd9e316a92934297590de14a67a1dc52
Philip Fisher-Ogden <pfishero+github@gmail.com>
2026-02-11 16:35:26 -0800

    fix(web-client): Update codegen to improve typing and fix cookie linting

--- web-client/lib/utils.ts
+++ web-client/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export function setCookie(name: string, value: string, maxAgeSeconds: number) {
+	document.cookie = `${name}=${value}; path=/; max-age=${maxAgeSeconds}`;
+}
