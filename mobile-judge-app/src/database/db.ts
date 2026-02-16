9b1fafcd69109fe3631cf8f1c0e7267d660c025a
PFO <pfisherogden@users.noreply.github.com>
2026-02-15 21:14:46 -0800

    undo: cleanup mobile-judge-app from main (#54)

--- mobile-judge-app/src/database/db.ts
+++
@@ -1,5 +0,0 @@
-// Shim for TypeScript resolution
-// This file is used when the bundler doesn't automatically pick up .web.ts or .native.ts
-// We default to web implementation for now (or could just throw errors)
-
-export * from './db.web';
