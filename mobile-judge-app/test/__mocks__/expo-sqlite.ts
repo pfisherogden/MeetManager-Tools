8dce387c2d000d21310bd26a987a2fe053cd9855
PFO <pfisherogden@users.noreply.github.com>
2026-02-15 21:15:28 -0800

    undo: cleanup mobile-judge-app from main (#54)

--- mobile-judge-app/test/__mocks__/expo-sqlite.ts
+++
@@ -1,6 +0,0 @@
-export const openDatabaseSync = jest.fn(() => ({
-  execSync: jest.fn(),
-  runSync: jest.fn((query, ...params) => ({ lastInsertRowId: 1, changes: 1 })),
-  getAllSync: jest.fn(() => []),
-  closeSync: jest.fn(),
-}));
