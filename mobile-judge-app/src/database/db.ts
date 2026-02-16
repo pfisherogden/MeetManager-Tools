27889fabcfcf66e74dc39fd96a05e5755392f6ec
PFO <pfisherogden@users.noreply.github.com>
2026-02-15 19:37:52 -0800

    feat: restore db.ts on main (#54)

---
+++ mobile-judge-app/src/database/db.ts
@@ -0,0 +1,5 @@
+// Shim for TypeScript resolution
+// This file is used when the bundler doesn't automatically pick up .web.ts or .native.ts
+// We default to web implementation for now (or could just throw errors)
+
+export * from './db.web';
