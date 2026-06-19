0b145ec06cb3b5b7ad89e3cc3a738ebd15649522
Philip Fisher-Ogden <pfishero+github@gmail.com>
2026-06-18 23:11:40 -0700

    Fix: Reduce column auto-resize padding to +5px (#502)

--- attendance-tracker/AttendanceTracker.js
+++ attendance-tracker/AttendanceTracker.js
@@ -214,7 +214,7 @@ function autoResizeAllSheets() {
         // Add padding to auto-resized columns so they don't look cramped
         for (var col = 1; col <= 13; col++) {
           var currentWidth = sheet.getColumnWidth(col);
-          sheet.setColumnWidth(col, currentWidth + 15);
+          sheet.setColumnWidth(col, currentWidth + 5);
         }
       }
     }
