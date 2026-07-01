423b0180e9e2ecc8875ce5207c11ac965ab0e616
PFO <pfisherogden@users.noreply.github.com>
2026-07-01 12:47:13 -0700

    fix(reporting): replace copyrighted Hy-Tek Meet Manager headers with MMTools (#541)

--- backend/src/mm_to_json/reporting/playwright_renderer.py
+++ backend/src/mm_to_json/reporting/playwright_renderer.py
@@ -62,7 +62,7 @@ class PlaywrightRenderer:
                         <div style="font-weight: bold;">Tri-Valley Swim Lg. C</div>
                     </div>
                     <div style="text-align: right;">
-                        <div>HY-TEK's MEET MANAGER 7.0 - {gen_time} &nbsp;&nbsp; Page <span class="pageNumber"></span></div>
+                        <div>MMTools - {gen_time} &nbsp;&nbsp; Page <span class="pageNumber"></span></div>
                     </div>
                 </div>
                 <div style="display: flex; justify-content: space-between; align-items: flex-end; width: 100%;">
