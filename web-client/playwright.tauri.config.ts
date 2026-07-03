ab870d245b035d699d1c94f44b64c29aa1ba8c16
Philip Fisher-Ogden <pfishero+github@gmail.com>
2026-07-02 18:09:06 -0700

    fix(e2e): exclude tauri_smoke test from standard Playwright configuration

--- web-client/playwright.tauri.config.ts
+++ web-client/playwright.tauri.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
 	testDir: "./tests-e2e",
+	testMatch: "**/tauri_smoke.spec.ts",
 	fullyParallel: false,
 	workers: 1,
 	reporter: "html",
